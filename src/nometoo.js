9d938d2a81e97e3d12a3a6d17c4efdeac382626f
Andreas Wurzer <andreas.wurzer@gmail.com>
2010-10-26 16:17:02 +0200

    changes:  * removed original  * added playlist functionality  * added tests

--- src/nometoo.js
+++ src/nometoo.js
@@ -1,26 +1,49 @@
-if (!iTunes) {
-	var iTunes = WScript.CreateObject("iTunes.Application");
+var copyPlaylist = function(playlist, destination, playlistDestination, deviceAlias, fileSystem) {
+    var playlistWriter = makePlaylistWriter(playlist, playlistDestination, deviceAlias, fileSystem);
+    copy(playlist.Tracks, destination, fileSystem, playlistWriter);
+    playlistWriter.close();
 }
 
-if (!fileSystem) {
-	var fileSystem = WScript.CreateObject("Scripting.FileSystemObject");
-}
+var makePlaylistWriter = function(playlist, playlistDestination, deviceAlias, fileSystem) {
+    var playlistFile = fileSystem.CreateTextFile(playlistDestination + "\\" + playlist.Name + ".m3u");
+    playlistFile.WriteLine("#EXTM3U");
 
-var copy = function(tracks, destination, fileSystem) {
-	var copyTrack = function(track) {
-		fileSystem.CopyFile(track.Location, buildTrackFolder(track), true);
-	};
+    var extractBaseName = function(location) {
+	return location.substring(location.lastIndexOf("\\") + 1, location.length);
+    };
 
-	var buildTrackFolder = function(track) {
-		return destination + "\\" + track.Artist + "\\" + track.Album;
-	};
-
-	for ( var i = 1; i <= tracks.Count; i++) {
-		var track = tracks.Item(i);
-		copyTrack(track);
+    return {
+	writeTrack: function(track) {
+	    playlistFile.WriteLine(buildTrackFolder(deviceAlias, track) + "\\" + extractBaseName(track.Location));
+	},
+	close: function() {
+	    playlistFile.Close();
 	}
+    };
+}
+
+var buildTrackFolder = function(destination, track) {
+    return destination + "\\" + track.Artist + "\\" + track.Album;
+};
+
+var copy = function(tracks, destination, fileSystem, playlistFile) {
+    var copyTrack = function(track) {
+	fileSystem.CopyFile(track.Location, buildTrackFolder(destination, track), true);
+    };
+
+    for ( var i = 1; i <= tracks.Count; i++) {
+	var track = tracks.Item(i);
+	copyTrack(track);
+	if(playlistFile) {
+	    playlistFile.writeTrack(track);
+	}
+    }
 };
 
 var selectedTracksFrom = function(musicLibrary) {
-	return musicLibrary.SelectedTracks;
+    return musicLibrary.SelectedTracks;
 };
+
+var selectedPlaylistFrom = function(musicLibrary) {
+    return musicLibrary.BrowserWindow.SelectedPlaylist;
+}
--- test/nometoo.js
+++ test/nometoo.js
@@ -19,22 +19,68 @@ var collectionFromArray = function(array) {
 
 var iTunes = {};
 iTunes.BrowserWindow = {};
-iTunes.SelectedTracks = collectionFromArray( [ track ]);
-var fileSystem = {
+iTunes.SelectedTracks = collectionFromArray([ track ]);
+iTunes.BrowserWindow.SelectedPlaylist = { Name: "Playlist", Tracks: collectionFromArray([ track ])};
+
+var makeFileSystem = function() {
+    return {
 	copied : [],
-	CopyFile : function(src, dest) {
-		this.copied.push( {
-			source : src,
-			destination : dest
-		});
+	playlist: {
+	    content: "",
+	    path: "",
+	    closed: false,
+	    WriteLine: function(line) {
+		this.content += line + "\r\n";
+	    },
+	    Close: function() {
+		this.closed = true;
+	    }
+	},
+	CreateTextFile: function(path) {
+	    this.playlist.path = path;
+	    return this.playlist;
+	},
+	CopyFile: function(src, dest) {
+	    this.copied.push( {
+		source : src,
+		destination : dest
+	    });
 	}
+    };
 };
 
 module("playlistWriter");
 
 test("copies selected tracks from iTunes to destination", function() {
-	copy(selectedTracksFrom(iTunes), "f:", fileSystem);
-	assertThat(fileSystem.copied.length, is(1));
-	assertThat(fileSystem.copied[0].source, is(track.Location));
-	assertThat(fileSystem.copied[0].destination, is("f:\\Disturbed\\Asylum"));
+    var fileSystem = makeFileSystem();
+    copy(selectedTracksFrom(iTunes), "f:", fileSystem);
+    assertThat(fileSystem.copied.length, is(1));
+    assertThat(fileSystem.copied[0].source, is(track.Location));
+    assertThat(fileSystem.copied[0].destination, is("f:\\Disturbed\\Asylum"));
+});
+
+test("creates playlist of selected playlist from iTunes", function() {
+    var fileSystem = makeFileSystem();
+    copyPlaylist(selectedPlaylistFrom(iTunes), "f:", "f:\\Playlists", "e:", fileSystem);
+    assertThat(fileSystem.playlist.path, is("f:\\Playlists\\Playlist.m3u"));
+});
+
+test("copies playlist tracks of selected playlist from iTunes", function() {
+    var fileSystem = makeFileSystem();
+    copyPlaylist(selectedPlaylistFrom(iTunes), "f:", "f:\\Playlists", "e:", fileSystem);
+    assertThat(fileSystem.copied.length, is(1));
+    assertThat(fileSystem.copied[0].source, is(track.Location));
+    assertThat(fileSystem.copied[0].destination, is("f:\\Disturbed\\Asylum"));    
+});
+
+test("writes tracks to Playlist", function () {
+    var fileSystem = makeFileSystem();
+    copyPlaylist(selectedPlaylistFrom(iTunes), "f:", "f:\\Playlists", "e:", fileSystem);
+    assertThat(fileSystem.playlist.content, is("#EXTM3U\r\ne:\\Disturbed\\Asylum\\04 - Another Way To Die.mp3\r\n"));
+});
+
+test("closes Playlist after write", function () {
+    var fileSystem = makeFileSystem();
+    copyPlaylist(selectedPlaylistFrom(iTunes), "f:", "f:\\Playlists", "e:", fileSystem);
+    assertThat(fileSystem.playlist.closed, is(true));
 });
