e9b69e5380374192d0874b87f2fe88fdd61c8761
Andreas Wurzer <andreas.wurzer@gmail.com>
2011-01-14 18:57:46 +0100

    replaced WScript.CreateObject with new ActiveXObject swapped functions

--- src/nometoo-starter.js
+++ src/nometoo-starter.js
@@ -1,15 +1,15 @@
-var iTunes = WScript.CreateObject("iTunes.Application");
-var fileSystem = WScript.CreateObject("Scripting.FileSystemObject");
-var shell = WScript.CreateObject("WScript.Shell");
+var iTunes = new ActiveXObject("iTunes.Application");
+var fileSystem = new ActiveXObject("Scripting.FileSystemObject");
+var shell = new ActiveXObject("WScript.Shell");
 var destDev = shell.RegRead("HKCU\\Software\\nometoo\\Destination");
 var playlistDest = shell.RegRead("HKCU\\Software\\nometoo\\PlaylistDestination");
 var devAlias = shell.RegRead("HKCU\\Software\\nometoo\\DeviceAlias");
 
+var logger = function(track) {
+    document.getElementById("progress").innerHtml = "kopiere " + track.Location;
+};
 
 var startCopy = function(form) {
     copy(selectedTracksFrom(iTunes), makeCopyFn(destDev + "\\" + form.trackFolder.value, fileSystem, shell, logger));
 };
 
-var logger = function(track) {
-    document.getElementById("progress").innerHtml = "kopiere " + track.Location;
-};
